2eb9b7307acadc4e616b352584f4a1dac148ba25
lalit2710 <ltk143.lk@gmail.com>
2022-10-06 17:43:01 +0530

    Code Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Product p = new Product();
+        p.product(4,5);
+        p.product(4,5,6);
+        p.product(7.8,3.9);
+    }
+}
+class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x,double y){
+        return x*y;
+    }
 }
